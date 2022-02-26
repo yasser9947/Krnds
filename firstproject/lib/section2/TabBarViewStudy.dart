3046e10e09ab58ba98042d37199925c9cbbd6b1e
yasser9947 <yasser9946.1@gmail.com>
2022-02-27 02:44:43 +0300

    TabBarViewStudy

--- firstproject/lib/section2/TabBarViewStudy.dart
+++ firstproject/lib/section2/TabBarViewStudy.dart
@@ -7,18 +7,48 @@ class TabBarViewStudy extends StatefulWidget {
   State<TabBarViewStudy> createState() => _TabBarViewStudyState();
 }
 
-class _TabBarViewStudyState extends State<TabBarViewStudy> {
+class _TabBarViewStudyState extends State<TabBarViewStudy> with SingleTickerProviderStateMixin {
+
+  late TabController myController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    myController = new TabController(length: 3, vsync: this , initialIndex: 1);
+
+    super.initState();
+  }
+
+
+
+// defoalt tap controller 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(length: 2, child: 
+    return
     
-    Scaffold(appBar: AppBar(title: Text("Homepage"),)
+    Scaffold(appBar: AppBar(
+      title: Text("Homepage"),
+      bottom: TabBar(
+        controller: myController,
+        isScrollable: false, 
+        indicatorColor: Colors.red,
+        indicatorWeight: 10,
+        indicatorPadding: EdgeInsets.all(5),
+        tabs: [
+        Tab(child: Text("ice"), icon: Icon(Icons.ice_skating),),
+        Tab(child: Text("heat"), icon: Icon(Icons.surfing),),
+         Tab(child: Text("spring"), icon: Icon(Icons.trending_neutral),)
+      ],),)
     ,
-    body: TabBarView(children: [
+    body: TabBarView(
+      controller: myController,
+      children: [
       Container(child: Text("View 1 "), width: double.infinity, color: Colors.green,),
-      Container(child: Text("View 2 "), width: double.infinity, color: Colors.blue,)
+      Container(child: Text("View 2 "), width: double.infinity, color: Colors.blue,),
+       Container(child: Text("View3 "), width: double.infinity, color: Colors.blue,)
+
     ],),
-    )
     );
+    
   }
 }
