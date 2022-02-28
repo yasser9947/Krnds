ecb546150c99ab28aa0b600ba096e2b7ec8b4794
yasser9947 <yasser9946.1@gmail.com>
2022-02-28 03:17:31 +0300

    س3

--- firstproject/lib/section3/TextFormFieldStudy.dart
+++ firstproject/lib/section3/TextFormFieldStudy.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 
@@ -11,6 +13,64 @@ class TextFormFieldStudy extends StatefulWidget {
 class _TextFormFieldStudyState extends State<TextFormFieldStudy> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text("homepage".toUpperCase()),),
+      body: Column(
+        // ignore: prefer_const_literals_to_create_immutables
+        children: [
+          TextField(
+            decoration: 
+            InputDecoration(
+
+//===============================================================================
+              icon: Icon(Icons.ac_unit_rounded),
+              labelText: "add your name",
+              // hintText: "add the naame" ,
+              // hintStyle: TextStyle(color: Colors.red , fontSize: 16),
+              hintMaxLines: 2,
+              // prefix 
+              prefixIcon: Icon(Icons.email),
+              prefix: Text("befor "),
+              //suffix
+              suffix: Text("end"),
+              //filled
+              filled: true,
+              fillColor: Colors.green.shade100,
+
+// ================================================================================
+
+              border:OutlineInputBorder( // UnderlineInputBorder
+                borderSide: BorderSide(color: Colors.pinkAccent)
+              ),
+              // الحالات 
+              enabled: true,
+              enabledBorder: OutlineInputBorder( // UnderlineInputBorder
+                borderSide: BorderSide(color: Colors.red)
+              ),
+              disabledBorder:  UnderlineInputBorder( // UnderlineInputBorder
+                borderSide: BorderSide(color: Colors.red)
+              ),
+
+              focusedBorder: OutlineInputBorder( // UnderlineInputBorder
+                borderSide: BorderSide(color: Colors.red),
+                borderRadius: BorderRadius.circular(20)
+              ),
+            ),
+
+
+// ====================================================================================
+
+            cursorColor: Colors.amber,
+            keyboardType: TextInputType.number, // to mak tthe kybors numbers 
+            maxLength: 20,
+            obscureText: true, // to type the passworsd
+            readOnly: true,
+            textInputAction: TextInputAction.search, // شكل الانتر في الكيبورد
+          ),
+      ],),
+
+
+    );
   }
+
 }
